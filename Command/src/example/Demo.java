754845da0ac7ed0a67ce2ef7f1a76584963b54cd
MarcosSouza671 <90912166+MarcosSouza671@users.noreply.github.com>
2023-10-23 20:18:43 -0300

    Upload

---
+++ Command/src/example/Demo.java
@@ -0,0 +1,10 @@
+package example;
+
+import editor.Editor;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
